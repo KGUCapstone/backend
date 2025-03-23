c67e323fa38ad2ac6d29af42fd9bb907708163dd
HanGwonhui <jana32496@gmail.com>
2025-03-23 21:21:28 +0900

    Feat: jwt token 구현, validation 로직

---
+++ src/main/java/com/kyonggi/backend/jwt/refresh/RefreshEntity.java
@@ -0,0 +1,22 @@
+package com.kyonggi.backend.jwt.refresh;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class RefreshEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    @Column(length = 1000)
+    private String refresh;
+    private String expiration;
+}
+
