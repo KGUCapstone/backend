25346c238e3ef0165dcd3ac461164193e0105b85
HanGwonhui <jana32496@gmail.com>
2025-03-23 21:23:46 +0900

    Feat: naver oauth2  구현

---
+++ src/main/java/com/kyonggi/backend/oauth2/CustomOAuth2UserService.java
@@ -0,0 +1,68 @@
+package com.kyonggi.backend.oauth2;
+
+import com.kyonggi.backend.model.member.entity.Member;
+import com.kyonggi.backend.model.member.repository.MemberRepository;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final MemberRepository userRepository;
+
+    public CustomOAuth2UserService(MemberRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        System.out.println(oAuth2User);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+        OAuth2Response oAuth2Response = null;
+        if (registrationId.equals("naver")) {
+            oAuth2Response = new NaverResponse(oAuth2User.getAttributes());
+        }
+
+        else {
+            return null;
+        }
+        String username = oAuth2Response.getProvider()+" "+oAuth2Response.getProviderId();
+        String name = oAuth2Response.getName();
+        Member existData = userRepository.findByUsername(username).orElse(null);
+
+        if (existData == null) {
+            Member userEntity = new Member();
+            userEntity.setUsername(username);
+            userEntity.setEmail(oAuth2Response.getEmail());
+            userEntity.setName(oAuth2Response.getName());
+            userEntity.setRole("ROLE_USER");
+
+            userRepository.save(userEntity);
+
+            UserDTO userDTO = new UserDTO();
+            userDTO.setUsername(username);
+            userDTO.setName(oAuth2Response.getName());
+            userDTO.setRole("ROLE_USER");
+
+            return new CustomOAuth2User(userDTO);
+        }
+        else {
+            existData.setEmail(oAuth2Response.getEmail());
+            existData.setName(oAuth2Response.getName());
+
+            userRepository.save(existData);
+
+            UserDTO userDTO = new UserDTO();
+            userDTO.setUsername(existData.getUsername());
+            userDTO.setName(oAuth2Response.getName());
+            userDTO.setRole(existData.getRole());
+
+            return new CustomOAuth2User(userDTO);
+        }
+    }
+}
---
+++ src/main/java/com/kyonggi/backend/oauth2/OAuth2Controller.java
@@ -0,0 +1,34 @@
+package com.kyonggi.backend.oauth2;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+
+@RestController
+@RequestMapping("/api/auth")
+public class OAuth2Controller {
+    @Value("${spring.backend.url}")
+    private String backendUrl;
+
+    @GetMapping("/naver")
+    public ResponseEntity<?> getNaverLoginUrl() {
+        String naverAuthUrl =
+                backendUrl+"/oauth2/authorization/naver";
+
+        return ResponseEntity.ok(naverAuthUrl);
+    }
+
+    @GetMapping("/token")
+    public ResponseEntity<?> getAccessToken(@CookieValue(value = "access_token", required = false) String accessToken) {
+        if (accessToken == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Access token not found");
+        }
+        return ResponseEntity.ok(Collections.singletonMap("accessToken", accessToken));
+    }
+}
