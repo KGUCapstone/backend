23582793ce8cf01a4976c868b27098dbf8d6277b
HanGwonhui <jana32496@gmail.com>
2025-03-29 20:49:52 +0900

    Feat: add member and getMember

--- src/main/java/com/kyonggi/backend/model/member/dto/CustomUserDetails.java
+++ src/main/java/com/kyonggi/backend/model/member/dto/CustomUserDetails.java
@@ -33,6 +33,10 @@ public class CustomUserDetails  implements UserDetails {
 
         return collection;
     }
+    public Member getMember() {
+
+        return member;
+    }
 
     public String getName() {
 
