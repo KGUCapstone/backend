598a579c8216fa53a93b540e007a6fa255d13938
한권희 <jana32496@gmail.com>
2025-05-07 13:51:31 +0900

    Revert "Revert "[Feat] 장바구니 기록 창 관련 필드 추가""

--- src/main/java/com/kyonggi/backend/model/cart/dto/CartSummaryDto.java
+++ src/main/java/com/kyonggi/backend/model/cart/dto/CartSummaryDto.java
@@ -12,11 +12,19 @@ public class CartSummaryDto {
     private Long cartId;
     private String name;
     private LocalDateTime createdAt;
+    private String thumbnailUrl;
+    private int totalQuantity;
+    private int totalPrice;
 
-    public CartSummaryDto(Long cartId, String name, LocalDateTime createdAt) {
+    public CartSummaryDto(Long cartId, String name, LocalDateTime createdAt,
+                          String thumbnailUrl, int totalQuantity, int totalPrice) {
         this.cartId = cartId;
         this.name = name;
         this.createdAt = createdAt;
+        this.thumbnailUrl = thumbnailUrl;
+        this.totalQuantity = totalQuantity;
+        this.totalPrice = totalPrice;
     }
+
 }
 
