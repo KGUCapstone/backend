2d8afe9054380a355085fb3b691d10b8cd7de279
HanGwonhui <jana32496@gmail.com>
2025-05-22 01:15:11 +0900

    Feat: CalendarSavedAmountDto

---
+++ src/main/java/com/kyonggi/backend/model/member/dto/GoalAmountRequest.java
@@ -0,0 +1,12 @@
+package com.kyonggi.backend.model.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GoalAmountRequest {
+    private int goalAmount;
+}
